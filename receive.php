317a359699260e992d38e34fecfbe6c5ea0db683
ninolin <a7872278722@gmail.com>
2018-01-06 10:54:42 +0800

    get line message

--- receive.php
+++ receive.php
@@ -1,3 +1,9 @@
 <?php
-	echo "test nino";
+	//接收request的body(可以接收除了Content-type為multipart/form-data的資料)
+	$json_str = file_get_contents('php://input'); 
+	$json_obj = json_decode($json_str); //轉成json格式
+	
+	$myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt，用來印訊息
+	fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加上\xEF\xBB\xBF轉成utf8格式
+	fclose($myfile);
 ?>
